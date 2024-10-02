56dde3b07de060facc58359b6eb3d76c353f9009
Emil <emilkle@stud.ntnu.no>
2024-10-02 22:08:18 +0200

    Added comments to placeholder.js and some logging functionality.

--- js/placeholder.js
+++ js/placeholder.js
@@ -2,15 +2,23 @@ let isFetching = false;
 let currentPage = 1;
 const postsPerPageLimit = 6;
 
+/**
+ * Fetches new posts from the API if the user has scrolled to the bottom of the page
+ */
 window.addEventListener("scroll", () => {
     if (window.innerHeight + window.scrollY >= document.body.offsetHeight && !isFetching) {
         fetchPosts();
     }
 });
 
+/**
+ * Fetches posts from the API
+ */
 function fetchPosts() {
+    // Check if the function is already fetching posts
     if (isFetching) return;
     isFetching = true;
+
     fetch('https://jsonplaceholder.typicode.com/posts?_page=' + currentPage + '&_limit=' + postsPerPageLimit)
         .then((response) => {
             if (!response.ok) {
@@ -19,10 +27,17 @@ function fetchPosts() {
             return response.json();
         })
         .then((posts) => {
+            // Log the fetched posts
+            console.log(`Fetched ${posts.length} posts from page ${currentPage}`);
             let container = document.getElementById("posts-container");
+
+
+            // Clear the container if the current page is 1
             if (currentPage === 1) {
                 container.innerHTML = "";
             }
+
+            // Append the fetched posts to the container
             let i = 1;
             for (post of posts) {
                 const apiPost = document.createElement("div");
@@ -37,6 +52,8 @@ function fetchPosts() {
                 i++;
             }
             isFetching = false;
+
+            // Increment the current page
             currentPage++;
         })
         .catch((error) => {
